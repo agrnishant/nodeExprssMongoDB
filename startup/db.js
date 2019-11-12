882ff34b6cad429617db0e9ab6dc9c590cdeba42
agrnishant <agrnishant@gmail.com>
2019-11-12 22:33:13 +0530

    db minor error correction

--- startup/db.js
+++ startup/db.js
@@ -5,3 +5,4 @@ mongoose.connect('mongodb://localhost:27017/nodeDB', {useUnifiedTopology: true,
   useNewUrlParser: true, useCreateIndex: true})
 .then(() => console.log('Connected to MongoDB...'))
 .catch(err => console.error(err));
+}
